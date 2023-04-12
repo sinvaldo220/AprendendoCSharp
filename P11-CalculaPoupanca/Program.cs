b39b61abf9db544aa4454239dcea7fe7de1bfb0e
Sinvaldo Lima <sinvaldo.lima@brpartners.com.br>
2023-04-11 22:15:10 -0300

    laços de repetição

---
+++ P11-CalculaPoupanca/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace P11_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("executando o projeto 11 - calcula poupança");
+
+            double investimento = 1000;
+            // redimento de 0.5% (0.005) ao mês
+            // mes 1
+            //investimento = investimento + investimento * 0.005;
+            // mes 2
+            //investimento = investimento + investimento * 0.005;
+            // mes 3
+            //investimento = investimento + investimento * 0.005;
+
+            int mes = 1;
+            while(mes <= 12)
+            {
+                investimento = investimento + investimento * 0.005;
+                Console.WriteLine(investimento);
+                mes = mes + 1;
+                //mes++;
+                //mes += 1;
+            }
+            //Console.WriteLine( "investimento" + investimento);
+            Console.WriteLine("Tewcle para fechar...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P12-CalculaPoupanca/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P12_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("executando o projeto 12 - calcula poupança 2");
+
+            double investimento = 1000;
+
+            for(int mes = 1; mes <= 12; mes++)
+            {
+                investimento = investimento + investimento * 0.005;
+                Console.WriteLine(investimento);
+                //mes++;
+                //mes += 1;
+            }
+            Console.WriteLine("Tewcle para fechar...");
+            Console.ReadLine();
+        }
+    }
+}
